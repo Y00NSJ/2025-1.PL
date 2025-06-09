51d58bf56747da3852ded5abfd448a9a40a690d6
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-10 03:13:57 +0900

    feat: 식과 전위 표기법 예제 코드 작성

---
+++ scheme/expr.scm
@@ -0,0 +1,8 @@
+;;; Scheme Basic
+;; <expr> -> <atom> | <list>
+;; <atom> -> number | string | id
+;; <list> -> '(' {<expr>} ')'
+
+;;expression is expressed by prefix notation
+(+ 1 3)			; 4
+(* (+ 2 4) (- 6 2))	; 24
